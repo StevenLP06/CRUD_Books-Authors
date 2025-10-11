19de861122b8e6ee7a6f5a7c122c1a0d4c5bcfc6
StevenLP06 <stlopezp@unal.edu.co>
2025-10-11 11:56:34 -0500

    feat: Added all methods for Book and store funcionality

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class BookController extends Controller
+{
+
+    public function index()
+    {
+        $books = Book::all();
+        return view('book')->with('books', $books);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        $book = new Book;
+        $book->title = $request->titulo;
+        $book->price = $request->precio;
+        $book->save();
+        return redirect()->route('books.index');
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+
+    public function edit($id)
+    {
+        $book = Book::find($id);
+        return view('book_edit')->with('book', $book);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $book = Book::find($id);
+        $book->title = $request->titulo;
+        $book->price = $request->precio;
+        $book->save();
+        return redirect()->route('books.index');
+    }
+
+    public function destroy($id)
+    {
+        $book = Book::find($id);
+        $book->delete();
+        return redirect()->route('books.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// RUTAS LIBROS
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
+Route::post('/books', [BookController::class, 'store'])->name('books.store');
+Route::get('/books/{id}', [BookController::class, 'edit'])->name('books.edit');
+Route::put('/books/{id}', [BookController::class, 'update'])->name('books.update');
+Route::delete('/books/{id}', [BookController::class, 'destroy'])->name('books.destroy');
